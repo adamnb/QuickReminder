876eef26350aef841c1a518bd1452fdf1178c264
adamnb <adamnathanielbrown@gmail.com>
2018-01-08 21:57:43 -0800

    First commit.

---
+++ QuickReminder/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace QuickReminder
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.titleText = new System.Windows.Forms.TextBox();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.descriptionText = new System.Windows.Forms.RichTextBox();
+            this.timeNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.elapsedTime = new System.Windows.Forms.Timer(this.components);
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.messageNotification = new System.Windows.Forms.NotifyIcon(this.components);
+            this.secondsNumeric = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pauseButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.timeNumeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondsNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(15, 120);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(57, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(12, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(27, 13);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Title";
+            this.titleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // titleText
+            // 
+            this.titleText.Location = new System.Drawing.Point(78, 6);
+            this.titleText.Name = "titleText";
+            this.titleText.Size = new System.Drawing.Size(182, 20);
+            this.titleText.TabIndex = 2;
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(12, 33);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(60, 13);
+            this.descriptionLabel.TabIndex = 3;
+            this.descriptionLabel.Text = "Description";
+            // 
+            // descriptionText
+            // 
+            this.descriptionText.Location = new System.Drawing.Point(79, 33);
+            this.descriptionText.Name = "descriptionText";
+            this.descriptionText.Size = new System.Drawing.Size(181, 57);
+            this.descriptionText.TabIndex = 4;
+            this.descriptionText.Text = "";
+            // 
+            // timeNumeric
+            // 
+            this.timeNumeric.Location = new System.Drawing.Point(55, 94);
+            this.timeNumeric.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.timeNumeric.Name = "timeNumeric";
+            this.timeNumeric.Size = new System.Drawing.Size(45, 20);
+            this.timeNumeric.TabIndex = 5;
+            this.timeNumeric.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "minutes and ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Every";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(78, 120);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(57, 23);
+            this.stopButton.TabIndex = 8;
+            this.stopButton.Text = "Stop\r\n";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // elapsedTime
+            // 
+            this.elapsedTime.Enabled = true;
+            this.elapsedTime.Interval = 1000;
+            this.elapsedTime.Tick += new System.EventHandler(this.elapsedTime_Tick);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(12, 148);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(71, 13);
+            this.timeLabel.TabIndex = 9;
+            this.timeLabel.Text = "Time Elapsed";
+            // 
+            // messageNotification
+            // 
+            this.messageNotification.Text = "notifyIcon1";
+            this.messageNotification.Visible = true;
+            // 
+            // secondsNumeric
+            // 
+            this.secondsNumeric.Location = new System.Drawing.Point(170, 94);
+            this.secondsNumeric.Name = "secondsNumeric";
+            this.secondsNumeric.Size = new System.Drawing.Size(34, 20);
+            this.secondsNumeric.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "seconds.";
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(141, 120);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(62, 23);
+            this.pauseButton.TabIndex = 12;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 171);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secondsNumeric);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.timeNumeric);
+            this.Controls.Add(this.descriptionText);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.titleText);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "main";
+            this.Text = "QuickReminder";
+            this.Load += new System.EventHandler(this.main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.timeNumeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondsNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox titleText;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.RichTextBox descriptionText;
+        private System.Windows.Forms.NumericUpDown timeNumeric;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Timer elapsedTime;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.NotifyIcon messageNotification;
+        private System.Windows.Forms.NumericUpDown secondsNumeric;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button pauseButton;
+    }
+}
+
---
+++ QuickReminder/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace QuickReminder
+{
+    public partial class main : Form
+    {
+
+        bool started = false;
+
+        enum timerState { running, stopped, paused }
+        timerState state = timerState.stopped;
+
+        public main()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (state != timerState.running)
+            {
+
+                if (titleText.Text == "")
+                {
+                    MessageBox.Show("Please enter a title."); //Dissalowing empty title field.
+                }
+                else
+                {
+                    startButton.Enabled = false;
+                    stopButton.Enabled = true;
+                    state = timerState.running;
+                }
+            }
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if ( state == timerState.running )
+            {
+                startButton.Enabled = true;
+                startButton.Text = "Resume";
+                pauseButton.Enabled = false;
+                state = timerState.paused;
+                
+            }
+  
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (state != timerState.stopped)
+            {
+                startButton.Enabled = true;
+                stopButton.Enabled = false;
+                state = timerState.stopped;
+            }
+        }
+
+        int t_i = 0; // Time elapsed
+        private void elapsedTime_Tick(object sender, EventArgs e)
+        {
+            if (state == timerState.running)
+            {
+                t_i++;
+                timeLabel.Text = t_i + " Seconds passed."; //Label displaying time
+
+                if (t_i % ((60 + secondsNumeric.Value) * timeNumeric.Value) == 1)
+                {
+
+                    if (t_i != 1)
+                    {
+
+                        if (descriptionText.Text == "")
+                        {
+                            // If description is left blank, it will be replaced by the title.
+                            descriptionText.Text = titleText.Text; 
+                        }
+
+                        //Notification Balloon
+                        messageNotification.Icon = SystemIcons.Application; // Icon for balloon
+
+                        messageNotification.BalloonTipTitle = titleText.Text; // Title
+                        messageNotification.BalloonTipText = descriptionText.Text; // Text
+
+                        messageNotification.ShowBalloonTip(1000); // Duration balloon is shown
+                    }
+                }
+            }
+            else if (state == timerState.stopped)
+            {
+                t_i = 0; // Reset time when stopped
+            }
+        }
+
+        
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
